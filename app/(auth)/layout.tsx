804bf2fa7aee379f3809bf07efdc5b3e6e7d6e70
Jan Eberwein <S2410629003@fhooe.at>
2025-06-14 10:11:31 +0200

    Recentering Auth Forms - allign Signup/in page to the middle

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <main>
+    <main className="flex items-center justify-center w-full min-h-screen px-6">
       {children}
     </main>
   );
